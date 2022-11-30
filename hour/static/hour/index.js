95d17bf4c520ce0fe410601eb266d7987ec1fcf3
Denisa Koscak <denisa.koscak@gmail.com>
2022-11-30 11:14:43 +0100

    update front-end logic

--- hour/static/hour/index.js
+++ hour/static/hour/index.js
@@ -7,86 +7,98 @@ function validateForm() {
 };
 
 
-
 document.querySelectorAll(".edit").forEach(button => {
     button.addEventListener('click', function(e){
         e.preventDefault();
         const day_id = button.value;
         const check_button = document.getElementById(`save_edit${day_id}`)
+        const cancel_button = document.getElementById(`cancel_edit${day_id}`)
         check_button.style.display = 'block';
-        document.querySelectorAll(`.time${day_id}`).forEach(row => {
+        cancel_button.style.display = 'block';
+        const all_inputs = document.querySelectorAll(`.time${day_id}`);
+        all_inputs.forEach(row => {
             row.querySelector('span').style.display = 'none';
             row.querySelector('input').style.display = 'block';
         })
         button.style.display = 'none';
-    
-        check_button.addEventListener('click', function(e) {
-            e.preventDefault();
-            const row = document.querySelector(`#row${day_id}`);
-            const start = row.querySelector(`#start${day_id}`).value;
-            const lunch_in = row.querySelector(`#lunch_in${day_id}`).value;
-            const lunch_out = row.querySelector(`#lunch_out${day_id}`).value;
-            const end = row.querySelector(`#end${day_id}`).value;
 
-            if (start && lunch_in){
-                if (lunch_out && end) {
-                    console.log("work all day");
-                }
-                else if (!lunch_out && !end){
-                    (console.log("work in the morning olny"));
-                }
-                else {
-                    console.log("incomplete day");
-                }
-            }
-            else if (!start && !lunch_in) {
-                if (lunch_out && end) {
-                    console.log("work in the afternoon");
-                }
-                else if (!lunch_out && !end){
-                    console.log("work not at all");
-                }
-                else {
-                    console.log("incomplete day");
-                }
-            }
-            else if (start && !lunch_in ) {
-                if (!lunch_out && end){
-                    console.log("work all day without launch pause");
-                }
-                else {
-                    console.log("incomplete day");
-                }
-            }
-            else {
-                console.log("incomplete day");
-            }
-            
-      
-            return false;
-        })
-        // const whole_form = document.querySelector('.update_time')
-        // whole_form.querySelectorAll('.time_start').defaultValue = "{{day.start | time:'H:i'}}"
-    })
+        cancel_button.addEventListener('click', function(){
+            all_inputs.forEach(row => {
+                row.querySelector('span').style.display = 'block';
+                row.querySelector('input').style.display = 'none';
+            })
+            check_button.style.display = 'none';
+            cancel_button.style.display = 'none';
+            button.style.display = 'block';
+        });
+    });
 });
 
-// document.querySelectorAll('.change_time').forEach(form => {
-//     form.addEventListener('submit', function(e) {
-//         e.preventDefault();
-//         console.log(form);
-//     })
-// })
 
-// function validateFormEdit() {
-//     alert("i am here")
-//     // e.preventDefault();
-//     // const date = document.forms['update_time'];
-//     const forms = document.querySelectorAll(".update_time")
-//     // const hhh = date['start'].value
-//     consolee.log(forms)
-//     if (date['start'].value === null || date['start'].value === "") {
-//         alert('Please choose a start time.');
-//         return false;
-//     }
-// };
- 
+function validateFormEdit(day_id) {
+            // e.preventDefault();
+    const row = document.querySelector(`#row${day_id}`);
+    const start = row.querySelector(`#start${day_id}`).value;
+    const lunch_in = row.querySelector(`#lunch_in${day_id}`).value;
+    const lunch_out = row.querySelector(`#lunch_out${day_id}`).value;
+    const end = row.querySelector(`#end${day_id}`).value;
+
+    if (start && lunch_in){
+        if (check_time(start, lunch_in)) {
+            if (lunch_out && end) {
+                if (check_time(lunch_in, lunch_out) && check_time(lunch_out, end)) {
+                    console.log("work all day");
+                } else {
+                    alert("wrong time input");
+                    return false;
+                }
+            } else if (!lunch_out && !end){
+                (console.log("work in the morning olny"));
+            } else {
+                alert("incomplete day");
+                return false;
+            }
+        } else {
+            alert("wrong time input");
+            return false;
+        }
+    } else if (!start && !lunch_in) {
+        if (lunch_out && end) {
+            if (check_time(lunch_out, end)) {
+                console.log("work in the afternoon");
+            } else {
+                alert("wrong time input");
+                return false;
+            }
+        } else if (!lunch_out && !end){
+            console.log("work not at all");
+        } else {
+            alert("incomplete day");
+            return false;
+        }
+    } else if (start && !lunch_in ) {
+        if (!lunch_out && end){
+            if (check_time(start, end)) {
+                console.log("work all day without launch pause");
+            } else {
+                alert("wrong time input");
+                return false;
+            }
+        } else {
+            alert("incomplete day");
+            return false;
+        }
+    } else {
+        alert("incomplete day");
+        return false;
+    }
+
+};
+
+function check_time(input_time, output_time) {
+    if (input_time > output_time) {
+        return false;
+    };
+    return true;
+};
+
